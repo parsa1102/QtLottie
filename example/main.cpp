9eca919cf72b785e37abfc499605a684d41ba0c8
Parsa Gholipour Shahraki <parsa.gholipour.sh@gmail.com>
2025-09-07 01:39:20 +0330

    add example

---
+++ example/main.cpp
@@ -0,0 +1,12 @@
+#include <LottieWidget.h>
+#include <QApplication>
+#include <thorvg.h>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    tvg::Initializer::init(4);   
+    Pari::LottieWidget widget("Animation.json");
+    widget.setPlay(true);
+    widget.show();
+    return app.exec();
+}
